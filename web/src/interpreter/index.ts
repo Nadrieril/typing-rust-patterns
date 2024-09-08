114a48769701f83043d94de5826df921bf0c1888
Nadrieril <nadrieril+git@gmail.com>
2024-09-08 14:00:11 +0200

    Remove unused code

--- web/src/interpreter/index.ts
+++ web/src/interpreter/index.ts
@@ -1,4 +1,3 @@
-
 /**
  * Wrapper of the Miri interpreter.
  * 
@@ -10,7 +9,6 @@ export class Interpreter {
   private readonly worker: Worker
   private onrun: (() => void)[];
   private onresult: ((result: string) => void)[]
-  private ondownloaded: ((name: string) => void)[];
   private onloaded: (() => void)[];
   
   /**
@@ -29,9 +27,8 @@ export class Interpreter {
     this.onrun = [];
     this.onresult = [];
     this.onloaded = [];
-    this.ondownloaded = [];
 
-    this.worker.onmessage = ({ data }: { data: { downloaded?: string, loaded?: boolean, result?: string } } ) => {
+    this.worker.onmessage = ({ data }: { data: { loaded?: boolean, result?: string } } ) => {
       if (data.result !== undefined) {
         for (const ev of this.onresult) {
           ev(data.result)
@@ -39,10 +36,6 @@ export class Interpreter {
       } else if (data.loaded !== undefined) {
         for (const ev of this.onloaded) {
           ev()
-        }
-      } else if (data.downloaded !== undefined) {
-        for (const ev of this.ondownloaded) {
-          ev(data.downloaded)
         }
       } else {
         console.log(`Received message`, data)
@@ -76,13 +69,6 @@ export class Interpreter {
    */
   public onResult(onresult: (result: string) => void) {
     this.onresult.push(onresult);
-  }
-  
-  /**
-   * @param ondownloaded Called when the Interpreter constructor has downloaded an asset.
-   */
-  public onAssetDownloaded(ondownloaded: (name: string) => void) {
-    this.ondownloaded.push(ondownloaded);
   }
   
   /**
