5d2d501612ac0c58c326ba4291da95bb0559c086
Nadrieril <nadrieril+git@gmail.com>
2024-09-08 10:16:00 +0200

    Run the solver live!

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 .PHONY: wasm
 wasm:
-	wasm-pack build --target web --release
+	rm -r web/typing_rust_patterns
+	wasm-pack build --target web --release -d web/typing_rust_patterns
 	# mv pkg/typing_rust_patterns_bg.wasm pkg/typing_rust_patterns_bg.big.wasm
 	# wasm-opt -Oz pkg/typing_rust_patterns_bg.big.wasm -o pkg/typing_rust_patterns_bg.wasm
--- src/wasm.rs
+++ src/wasm.rs
@@ -3,10 +3,9 @@ use wasm_bindgen::prelude::wasm_bindgen;
 use crate::*;
 
 #[wasm_bindgen]
-pub fn trace_solver_str(request: &str) {
+pub fn trace_solver_str(request: &str) -> String {
     let options = RuleOptions::DEFAULT;
     let a = &Arenas::default();
     let req = TypingRequest::parse(a, request).unwrap();
-    let trace = trace_solver(req, options, PredicateStyle::Sequent);
-    web_sys::console::log_1(&trace.into());
+    trace_solver(req, options, PredicateStyle::Sequent)
 }
