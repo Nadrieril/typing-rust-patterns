dbc67b9681a30a0c9d1e74ef3d70df022a9710c0
Nadrieril <nadrieril+git@gmail.com>
2024-09-08 16:18:28 +0200

    Reorganize worker

--- web/src/solver/index.ts
+++ web/src/solver/index.ts
@@ -6,66 +6,69 @@
  * To interact with the Solver use the `run` and `onResult` methods.
  */
 export class Solver {
-  private readonly worker: Worker
-  private onresult: ((result: string) => void)[]
-  private onloaded: (() => void)[];
-  private running: bool;
-  
-  /**
-   * Creates a new Solver.
-   * 
-   * The constructor is asynchronous, check `onLoaded` for status on when it can be used.
-   * 
-   * @example
-   * const solver = new Solver();
-   * solver.onLoaded(() => {
-   *   solver.run('println!("Hello, World!");');
-   * })
-   */
-  constructor() {
-    this.worker = new Worker(new URL("./worker.ts", import.meta.url), { type: "module" });
-    this.onresult = [];
-    this.onloaded = [];
-    this.running = false;
+    private readonly worker: Worker
+    private onresult: ((result: string) => void)[]
+    private onloaded: (() => void)[];
+    private running: bool;
 
-    this.worker.onmessage = ({ data }: { data: { loaded?: boolean, result?: string } } ) => {
-      if (data.result !== undefined) {
+    /**
+     * Creates a new Solver.
+     * 
+     * The constructor is asynchronous, check `onLoaded` for status on when it can be used.
+     * 
+     * @example
+     * const solver = new Solver();
+     * solver.onLoaded(() => {
+     *   solver.run('println!("Hello, World!");');
+     * })
+     */
+    constructor() {
+        this.worker = new Worker(new URL("./worker.ts", import.meta.url), { type: "module" });
+        this.onresult = [];
+        this.onloaded = [];
         this.running = false;
-        for (const ev of this.onresult) {
-          ev(data.result)
-        }
-      } else if (data.loaded !== undefined) {
-        for (const ev of this.onloaded) {
-          ev()
-        }
-      }
-    };
-  }
 
-  public is_running() {
-      return this.running;
-  }
+        this.worker.onmessage = ({ data }) => {
+            switch (data.event) {
+                case "loaded":
+                    for (const ev of this.onloaded) {
+                        ev()
+                    }
+                    break;
+                case "result":
+                    for (const ev of this.onresult) {
+                        ev(data.result)
+                    }
+                    this.running = false;
+                    break;
+            }
+        };
+    }
 
-  public run_solver(code: string) {
-    this.running = true;
-    this.worker.postMessage({call: "run_solver", code});
-  }
+    public is_running() {
+        return this.running;
+    }
 
-  public set_key(key: string, value: string) {
-    this.worker.postMessage({call: "set_key", key, value});
-  }
+    public run_solver(code: string) {
+        this.running = true;
+        this.worker.postMessage({call: "run_solver", code});
+    }
 
-  /**
-   * @param onloaded Called when the Solver has finished loading.
-   */
-  public onLoaded(onloaded: () => void) {
-    this.onloaded.push(onloaded);
-  }
+    public set_key(key: string, value: string) {
+        this.worker.postMessage({call: "set_key", key, value});
+    }
 
-  /**
-   * @param onresult Called when the Solver has finished executing code and has a result.
-   */
-  public onResult(onresult: (result: string) => void) {
-    this.onresult.push(onresult);
-  }
+    /**
+     * @param onloaded Called when the Solver has finished loading.
+     */
+    public onLoaded(onloaded: () => void) {
+        this.onloaded.push(onloaded);
+    }
+
+    /**
+     * @param onresult Called when the Solver has finished executing code and has a result.
+     */
+    public onResult(onresult: (result: string) => void) {
+        this.onresult.push(onresult);
+    }
 }
--- web/src/solver/worker.ts
+++ web/src/solver/worker.ts
@@ -1,53 +1,36 @@
 import init, { RuleOptions, trace_solver_str } from "../../typing_rust_patterns/typing_rust_patterns.js";
 
-export async function initSolver(): Promise<Solver> {
-  console.time("init");
-  await init({});
-  console.timeEnd("init");
-  return new Solver();
-}
-
-class Solver {
-  options: RuleOptions;
-
-  constructor() {
-    this.options = new RuleOptions();
-  }
-
-  async run(code: string): Promise<string> {
-    console.time("solver execution");
-    try {
-      return trace_solver_str(code, this.options)
-
-    } catch (e: any) {
-      return e.message;
-
-    } finally {
-      console.timeEnd("solver execution");
-    };
-  }
-
-  set_key(key: string, value: string) {
-    console.log("Setting key", key, ":", value);
-    this.options.set_key(key, value);
-  }
-}
-
 (async () => {
-  // Build main Solver
-  const solver = await initSolver();
-  
-  // When code is received run it
-  addEventListener("message", async (event) => {
-    const data = event.data;
-    if (data.call == "set_key") {
-      solver.set_key(data.key, data.value);
-    } else if (data.call == "run_solver") {
-      const result = await solver.run(data.code);
-      postMessage({ result });
-    }
-  });
+    console.time("init");
+    await init({});
+    console.timeEnd("init");
 
-  // Send a message when finished loading
-  postMessage({ loaded: true })
+    let options = new RuleOptions();
+
+    // When code is received run it
+    addEventListener("message", async (event) => {
+        const data = event.data;
+        switch (data.call) {
+            case "set_key":
+                console.log("Setting key", data.key, ":", data.value);
+                options.set_key(data.key, data.value);
+                break;
+
+            case "run_solver":
+                console.time("run_solver");
+                let result = "unknown error";
+                try {
+                    result = trace_solver_str(data.code, options);
+                } catch (e: any) {
+                    result = e.message;
+                } finally {
+                    console.timeEnd("run_solver");
+                };
+                postMessage({ event: "result", result });
+                break;
+        }
+    });
+
+    // Send a message when finished loading
+    postMessage({ event: "loaded" })
 })()
