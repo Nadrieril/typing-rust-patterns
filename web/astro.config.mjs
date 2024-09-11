dcb4e0826c857759f8840f75e4451f7a23a71844
Nadrieril <nadrieril+git@gmail.com>
2024-09-11 21:31:58 +0200

    Deploy with GitHub Actions

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,8 +5,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://garriga.dev",
-  base: "/trup",
+  site: "https://nadrieril.github.io",
+  base: "/typing-rust-patterns",
   integrations: [react(), compress(), compressor({
     brotli: false,
     fileExtensions: [".css", ".js", ".html", ".xml", ".cjs", ".mjs", ".svg", ".txt", ".wasm"]
