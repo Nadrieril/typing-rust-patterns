886e73ee36fd8e1e24a57f5989578f9f6473865b
Nadrieril <nadrieril+git@gmail.com>
2024-09-17 11:30:10 +0200

    Try to fix CI

--- web/src/components/worker.ts
+++ web/src/components/worker.ts
@@ -1,27 +1,29 @@
 import init, { RuleSetJs, compare_rulesets_js } from "../../typing_rust_patterns/typing_rust_patterns.js";
 
-await init({});
+(async () => {
+    await init({});
 
-const truncateAt = 300;
+    const truncateAt = 300;
 
-addEventListener("message", async (event) => {
-    const data = event.data;
-    switch (data.type) {
-        case "compare":
-            const output = compare_rulesets_js(
-                RuleSetJs.decode(data.optionsLeft)!,
-                RuleSetJs.decode(data.optionsRight)!,
-                data.patDepth,
-                data.tyDepth,
-            );
-            if (output.length > truncateAt) {
-                const diff = output.length - truncateAt;
-                output.length = truncateAt;
-                output.push({ req: `and ${diff} more...` })
-            }
-            postMessage({ type: "compare", output });
-            break;
-    }
-});
+    addEventListener("message", async (event) => {
+        const data = event.data;
+        switch (data.type) {
+            case "compare":
+                const output = compare_rulesets_js(
+                    RuleSetJs.decode(data.optionsLeft)!,
+                    RuleSetJs.decode(data.optionsRight)!,
+                    data.patDepth,
+                    data.tyDepth,
+                );
+                if (output.length > truncateAt) {
+                    const diff = output.length - truncateAt;
+                    output.length = truncateAt;
+                    output.push({ req: `and ${diff} more...` })
+                }
+                postMessage({ type: "compare", output });
+                break;
+        }
+    });
 
-postMessage({ type: "loaded" });
+    postMessage({ type: "loaded" });
+})()
